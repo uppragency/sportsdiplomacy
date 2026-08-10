35e28d5d479afee74630f7359692eaf30ef1c90d
uppragency <upprminds@gmail.com>
2026-08-10 22:08:20 +0300

    v1.8

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 
 const NAV_LINKS = [
   { href: '#despre', label: 'Despre' },
-  { href: '#speakeri', label: 'Speakeri' },
   { href: '#galerie', label: 'Galerie' },
   { href: '#media', label: 'Media' },
   { href: '#sponsori', label: 'Sponsori' },
